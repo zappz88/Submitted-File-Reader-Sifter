3272e8f159be3d85a97c8772001d43734c608457
zappz88 <31833538+zappz88@users.noreply.github.com>
2019-03-29 14:52:40 -0500

    Added ability to sift through xml files
    Was found necessary to typecast objects into  arrays first and then ascribing values to variables to avoid quotation mark/ apostrophe conflict in sql query string. May be able to use interpolation, but currently the function works.

--- php_fileuploader2.php
+++ php_fileuploader2.php
@@ -1,7 +1,7 @@
 <?php
 
-if (isset($_FILES['fileToUpload2'])) {
-    $file = $_FILES['fileToUpload2'];
+if (isset($_FILES['fileToUpload3'])) {
+    $file = $_FILES['fileToUpload3'];
     $file;
 
     //file properties
@@ -11,45 +11,77 @@ if (isset($_FILES['fileToUpload2'])) {
     $file_error = $file['error'];
 
     //file extensions
-    $file_ext = explode('.', $file_name);
-    $file_ext = strtolower(end($file_ext));
-    $allowed = array("txt", 'jpg');
+    $file_exploded = explode('.', $file_name);
+    $file_ext = strtolower(end($file_exploded));
+    $allowed = array("txt", "xml");
 
     /* Check to see if file type is allowed for upload via $allowed, and if so,
       break on requested character */
     if (in_array($file_ext, $allowed)) {
-        if ($file_error === 0) {
-            if ($file_size <= 2097152) {
-//                 $file_name_new = uniqid('', true) . "." . $file_ext;
-//                 $file_destination = 'uploads/' . $file_name_new;
-//                 
-//                 if(move_uploaded_file($file_tmp, $file_destination)){
-//                     $file_destination;
-//                 }
-                $myfile = fopen($file_tmp, "r") or die("Unable to open file!");
-                $readfile = fread($myfile, filesize($file_tmp));
-                $exploded = explode('.', $readfile);
-//                 print_r($exploded);
-                fclose($myfile);
-//                 print_r($exploded[0]);
+        if ($file_ext == 'txt') {
+            if ($file_error === 0) {
+                if ($file_size <= 2097152) {
+                    $myfile = fopen($file_tmp, "r") or die("Unable to open file!");
+                    if ($myfile) {
+                        $count = 0;
+                        while (($buffer = fgets($myfile)) !== false) {
+                            $exploded = explode("|", $buffer);
 
-                $servername = "";
-                $username = "";
-                $password = "";
-                $dbname = "test";
+                            $servername = "localhost";
+                            $username = "root";
+                            $password = "";
+                            $dbname = "mydb";
 // Create connection
-                $conn = mysqli_connect($servername, $username, $password, $dbname);
+                            $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-                if (!$conn) {
-                    die("Connection failed: " . mysqli_connect_error());
-                }
+                            if (!$conn) {
+                                die("Connection failed: " . mysqli_connect_error());
+                            }
 
-                foreach ($exploded as $value) {
-                    $sql = "insert into line(sentence) values('$value');";
-                    mysqli_query($conn, $sql);
+                            $sql = "insert into uploads(line1, line2, line3) values('$exploded[0]', '$exploded[1]', '$exploded[2]');";
+                            mysqli_query($conn, $sql);
+                            mysqli_close($conn);
+                        }
+                    }
                 }
-                mysqli_close($conn);
+                if (!feof($myfile)) {
+                    echo "Error: unexpected fgets() fail\n";
+                }
+                fclose($myfile);
+            }
+        }
+        if ($file_ext == 'xml') {
+            if ($file_error === 0) {
+                if ($file_size <= 2097152) {
+                    if ($file_tmp) {
+                        $count = 0;
+                        $xml = simplexml_load_file($file_tmp) or die("Error: Cannot create object");
+                        for ($i = 0; $i < sizeof($xml->person); $i++) {
+                            $person = $xml->person[$i];
+                            $person = (array) $person;
+                            $firstName = $person['firstname'];
+                            $middleName = $person['middlename'];
+                            $lastName = $person['lastname'];
+
+                            $servername = "localhost";
+                            $username = "root";
+                            $password = "";
+                            $dbname = "mydb";
+// Create connection
+                            $conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+                            if (!$conn) {
+                                die("Connection failed: " . mysqli_connect_error());
+                            }
+
+                            $sql = "insert into uploads(line1, line2, line3) values('$firstName', '$middleName', '$lastName');";
+                            mysqli_query($conn, $sql);
+                            mysqli_close($conn);
+                        }
+                    }
+                }
             }
         }
     }
-}    
+}
+?>
